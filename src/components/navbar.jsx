bb00269ca8734cbd72e48cc1b79d771094e03321
Raghav Agarwal <144090291+Phoenix-tear@users.noreply.github.com>
2024-03-31 16:04:52 +0530

    basic bug fix

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.css";
 import LinkModal from "./linkmodal";
 import LinkImg from "./linkimg"; 
 import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css'
+import 'tippy.js/dist/tippy.css';
 
-function Navbar({ handleBold, handleItalics, handleHeadings, isLinkModalOpen, setIsLinkModalOpen, isImageModalOpen, setIsImageModalOpen, handleLinkSubmit, handleCodeblock}) {
+function Navbar({ handleBold, handleItalics, handleHeadings, isLinkModalOpen, setIsLinkModalOpen, isImageModalOpen, setIsImageModalOpen, handleLinkSubmit, handleCodeblock }) {
     const [showHeadingTooltip, setShowHeadingTooltip] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     const handleLinkButtonClick = () => {
         setIsLinkModalOpen(true);
@@ -18,6 +30,11 @@ function Navbar({ handleBold, handleItalics, handleHeadings, isLinkModalOpen, se
         setIsLinkModalOpen(false); 
     };
 
+    const headingmanager = () => {
+        setShowHeadingTooltip(false);
+        handleHeadings();
+    };
+
     return (
         <div className="h-[10vh] navbar md:px-[2vw] py-[2vh] flex text-s w-full md:gap-x-[5vw] lg:gap-x-[5vw] md:text-lg lg:text-lg justify-evenly relative md:justify-normal">
             <div className="container w-max relative hover:scale-110 duration-150 ease-in-out">
@@ -27,21 +44,28 @@ function Navbar({ handleBold, handleItalics, handleHeadings, isLinkModalOpen, se
                 <button onClick={handleItalics}>Italics</button>
             </div>
             <div className="container w-max relative hover:scale-110 duration-150 ease-in-out">
-                <Tippy content="Select the text with a space before it to enable heading" className=" bg-slate-200 absolute w-[15vw]">
-                    <button
-                        onMouseEnter={() => setShowHeadingTooltip(true)} 
-                        onMouseLeave={() => setShowHeadingTooltip(false)} 
-                        onClick={handleHeadings}
-                    >
+                {!isMobile && (
+                    <Tippy content="Select the text with a space before it to enable heading" className=" bg-slate-200 absolute w-[15vw]">
+                        <button
+                            onMouseEnter={() => setShowHeadingTooltip(true)} 
+                            onMouseLeave={() => setShowHeadingTooltip(false)} 
+                            onClick={headingmanager}
+                        >
+                            Heading
+                            {showHeadingTooltip && ( 
+                                <div className="tooltip"></div>
+                            )}
+                        </button>
+                    </Tippy>
+                )}
+                {isMobile && (
+                    <button onClick={headingmanager}>
                         Heading
-                        {showHeadingTooltip && ( 
-                            <div className="tooltip"></div>
-                        )}
                     </button>
-                </Tippy >
+                )}
             </div>
             <div className="container w-max relative hover:scale-110 duration-150 ease-in-out">
-            <button onClick={handleCodeblock}>Codeblock</button>
+                <button onClick={handleCodeblock}>Codeblock</button>
             </div>
             <div className="flex flex-col w-max relative ">
                 <button className="linkbtn hover:scale-110 duration-150 ease-in-out" onClick={handleLinkButtonClick}>Link</button> 
